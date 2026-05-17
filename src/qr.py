5a2f3d9a6868671afa58eb4970c64ec56cb59130
LedApalachee <ledapalachee@gmail.com>
2026-05-17 18:42:54 +0500

    первая работаящая версия REST API, способная принимать запросы

---
+++ src/qr.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from dotenv import load_dotenv
+import database as db
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+api_url = os.getenv("API_URL")
+
+def get_item_names_by_qrraw(qrraw: str) -> list[str]:
+    item_names = []
+    items_db = []
+    rbody = {"token":api_key, "qrraw":qrraw}
+    r = requests.post(api_url, data=rbody)
+    print("\n\n\nljl")
+    print(r.json())
+    for item in r.json()['data']['json']['items']:
+        # формируем список названий товаров для отправки на фронт
+        item_names += [item['name']]
+        
+        # формируем список товаров для сохранения в БД
+        item_db = db.Product()
+        item_db.name = item['name']
+        items_db += [item_db]
+
+    db.add_product(items_db)
+    return item_names
--- src/rest.py
+++ src/rest.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import database as db
+import qr
+
+class QRText(BaseModel):
+    qrraw: str
 
 app = FastAPI()
 
+# возвращает список ВСЕХ позиций, хранящихся в БД
+# возвращает список СТРОК - названий товаров
+@app.get("/items")
+async def pull_items():
+    items = []
+    for item in db.pull_products():
+        items += [item.name]
+    return items
 
-class Item(BaseModel):
-    name: str
-    price: float
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI!"}
-
-
+# прогон расшифровки QR-кода через API для получения списка товаров
+# добавляем эти товары в БД + возвращаем прочитанный список на фронт
 @app.post("/items")
-async def create_item(item: Item):
-    return {
-        "status": "success",
-        "item": item.name
-    }
+async def pass_qrcode_text(qrcode_text: QRText):
+    return qr.get_item_names_by_qrraw(qrcode_text.qrraw)
