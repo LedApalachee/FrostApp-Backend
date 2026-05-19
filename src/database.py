b19b238744aba411fc2634c930c608e493754414
LedApalachee <ledapalachee@gmail.com>
2026-05-19 20:38:05 +0500

    новый REST + временная заглушка для БД + обновил спецификацию и readme

--- src/database.py
+++ src/database.py
@@ -14,10 +14,54 @@ class Product(Base):
 
 Base.metadata.create_all(engine)
 
-def add_product(products):
-    session.add_all(products)
-    session.commit()
+users_db = []
+users_next_id = 0
 
-def pull_products():
-    products = session.query(Product).all()
-    return products
+items_db = []
+items_next_id = 0
+
+
+def find_by_email(email: str):
+    for user in users_db:
+        if user["email"] == email:
+            return user
+    return None
+
+
+def create_user(username: str, email: str, passhash: str):
+    global users_next_id
+    users_db.append({"id": users_next_id, "name": username, "email": email, "passhash": passhash})
+    users_next_id += 1
+    return users_db[-1]["id"]
+
+
+def get_user(user_id: int):
+    for user in users_db:
+        if user["id"] == user_id:
+            return user
+    return None
+
+
+def add_items(user_id: int, items: list):
+    global items_next_id
+    for item in items:
+        items_db.append({"id": items_next_id, "user_id": user_id, "name": item["name"], "expiration": item["expiration"], "deleted": False})
+        items_next_id += 1
+    return "success"
+
+
+def get_items(user_id: int) -> list:
+    found = []
+    for item in items_db:
+        if item["user_id"] == user_id:
+            found.append(item)
+    return found
+
+
+def items_to_delete(item_ids: list[int]):
+    i = 0
+    for item in items_db:
+        if item["id"] in item_ids:
+            item["deleted"] = True
+            i += 1
+    return f"{i} items deleted"
--- src/rest.py
+++ src/rest.py
@@ -1,25 +1,108 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
+from dotenv import load_dotenv
 import database as db
 import qr
+import jwt
+import os
+
 
 class QRText(BaseModel):
+    user_id: int
     qrraw: str
+    token: str
+
+class Register(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+class NewItems(BaseModel):
+    user_id: int
+    items: list
+    token: str
+
+class DropItems(BaseModel):
+    item_ids: list[int]
+    user_id: int
+    token: str
+
 
 app = FastAPI()
+load_dotenv()
 
-# возвращает список ВСЕХ позиций, хранящихся в БД
-# возвращает список СТРОК - названий товаров
+
+# Регистрация нового пользователя
+@app.post("/users")
+def new_user(user: Register):
+    if db.find_by_email(user.email):
+        return {"message": "email exists"}
+    user_id = db.create_user(user.name, user.email, user.password)
+    return {
+        "message": "success",
+        "user_id": user_id,
+        "token": jwt.encode({"user_id": user_id}, os.getenv("SECRET_KEY"), algorithm="HS256")
+    }
+
+
+@app.get("/users")
+def get_user(user_id: int, token: str):
+    if token != jwt.encode({"user_id": user_id}, os.getenv("SECRET_KEY"), algorithm="HS256"):
+        return {"message": "bad token"}
+    user = db.get_user(user_id)
+    if not user:
+        return {"message": "user not found"}
+    return {
+        "message": "success",
+        "user": {"id": user["id"], "name": user["name"], "email": user["email"]}
+    }
+
+
+# Логин
+@app.post("/login")
+def login(login_form: Login):
+    user = db.find_by_email(login_form.email)
+    if user and user["passhash"] == login_form.password:
+        return {
+            "message": "success",
+            "user_id": user["id"],
+            "token": jwt.encode({"user_id": user["id"]}, os.getenv("SECRET_KEY"), algorithm="HS256")
+        }
+    return {"message": "incorrect"}
+
+
+# Список продуктов данного пользователя
 @app.get("/items")
-async def pull_items():
-    items = []
-    for item in db.pull_products():
-        items += [item.name]
-    return items
+def get_items(user_id: int, token: str):
+    if token == jwt.encode({"user_id": user_id}, os.getenv("SECRET_KEY"), algorithm="HS256"):
+        return {"items": db.get_items(user_id)}
+    return {"message": "bad token"}
 
 
-# прогон расшифровки QR-кода через API для получения списка товаров
-# добавляем эти товары в БД + возвращаем прочитанный список на фронт
+# Добавить продукты этого пользователя
 @app.post("/items")
-async def pass_qrcode_text(qrcode_text: QRText):
-    return qr.get_item_names_by_qrraw(qrcode_text.qrraw)
+def add_items(newitems: NewItems):
+    if newitems.token == jwt.encode({"user_id": newitems.user_id}, os.getenv("SECRET_KEY"), algorithm="HS256"):
+        db.add_items(newitems.user_id, newitems.items)
+        return {"message": "success"}
+    return {"message": "bad token"}
+
+
+# Сканировать QR-код этого пользователя
+@app.post("/qr-text")
+def scan_qrtext(qrdata: QRText):
+    if qrdata.token == jwt.encode({"user_id": qrdata.user_id}, os.getenv("SECRET_KEY"), algorithm="HS256"):
+        return qr.get_item_names_by_qrraw(qrdata.qrraw)
+    return {"message": "bad token"}
+
+
+# Удалить данные продукты
+@app.post("/items/delete")
+def delete_items(dropitems: DropItems):
+    if dropitems.token == jwt.encode({"user_id": dropitems.user_id}, os.getenv("SECRET_KEY"), algorithm="HS256"):
+        return {"message": db.items_to_delete(dropitems.item_ids)}
+    return {"message": "bad token"}
