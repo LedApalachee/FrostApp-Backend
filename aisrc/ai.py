ea36e8f4bd0afd04af11721e5a39ba4bbe5f0225
KorGeo1 <korgeovlad@gmail.com>
2026-05-20 12:23:58 +0500

    update info about AI, libraries

--- aisrc/ai.py
+++ aisrc/ai.py
@@ -1,18 +1,7 @@
-import requests
-import json
-import os
+import lmstudio as lms
 
-response = requests.post(
-  "http://localhost:1234/api/v1/chat",
-  headers={
-    "Authorization": f"Bearer {os.environ['LM_API_TOKEN']}",
-    "Content-Type": "application/json"
-  },
-  json={
-    "model": "google/gemma-4-e4b",
-    "input": "Write a short haiku about sunrise."
-  }
-)
-print(json.dumps(response.json(), indent=2))
+# lms.configure_default_client(api_token="sk-lm-ITCeAy2V:QiOWmkGIkKUjcgAYFmbU")
 
-
+model = lms.llm("google/gemma-4-e4b")
+result = model.respond("What is the meaning of life?")
+print(result)
--- aisrc/i.py
+++
@@ -1,7 +0,0 @@
-import lmstudio as lms  
-
-#lms.configure_default_client(api_token="sk-lm-ITCeAy2V:QiOWmkGIkKUjcgAYFmbU")
-
-model =lms.llm("google/gemma-4-e4b")
-result = model.respond("What is the meaning of life?")
-print(result)
